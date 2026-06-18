3eca178bacb53aff12f1a7bde52b98ac19e72038
Lewis Jones <lewisjones1809@gmail.com>
2026-06-18 20:33:36 +0100

    function updates, getter added and purchase/sale functions added

---
+++ functions.py
@@ -0,0 +1,154 @@
+import sqlite3
+import requests
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+# call the api and return the response as a json object
+def call_api(params: dict, id: str ="") -> dict:
+    # load .env file
+    load_dotenv()
+
+    # set URL and API key
+    url = f"https://api.pokemontcg.io/v2/cards/{id}"
+    api_key = os.getenv("POKEMON_API_KEY")
+
+    # call the API and get the response
+    response = requests.get(url, headers={"X-Api-Key": api_key}, params=params)
+    return response
+
+# add new card to allCards table
+def create_new_cards(cur: sqlite3.Cursor, cards: dict[dict]) -> None:
+    # initialise counters for new cards and variants
+    new_cards_counter = 0
+    new_variants_counter = 0
+
+    # loop through all cards
+    for card in cards:
+
+        # check if cardmarket data exists for the card
+        cardmarket = card.get('cardmarket')
+
+        #if data exists, extract url, otherwise set them to None
+        if cardmarket is not None:
+            url = cardmarket.get('url')
+            has_reverse_holo = (cardmarket.get('prices').get('reverseHoloAvg30') != 0)
+        else:
+            url = None
+            has_reverse_holo = False
+
+        # if card not in table already, insert into allCards table
+        if cur.execute("SELECT id FROM allCards WHERE id = ?", (card['id'],)).fetchone() is None:
+            cur.execute("INSERT INTO allCards (id, cardName, setName, setNumber, url, rarity) VALUES (?,?, ?, ?, ?, ?)", (card['id'], card['name'], card['set']['name'], card['number'], url, card.get('rarity', 'Unknown')))
+            new_cards_counter += 1
+
+        # if variant not in table already, insert into cardVariants table
+        if cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card['id'], 'Normal')).fetchone() is None:    
+            cur.execute("INSERT INTO cardVariants (cardID, finish) VALUES (?, ?)", (card['id'], 'Normal'))
+            new_variants_counter += 1
+    
+        # if reverse holo exists and not in table already, insert into cardVariants table
+        if has_reverse_holo and cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card['id'], 'Reverse Holo')).fetchone() is None:
+            cur.execute("INSERT INTO cardVariants (cardID, finish) VALUES (?, ?)", (card['id'], 'Reverse Holo'))
+            new_variants_counter += 1
+
+    # feed back to the user how many new cards and variants were added to the database
+    print(f"Inserted {new_cards_counter} new cards and {new_variants_counter} new variants into the database.")
+
+# update price history
+def update_prices(cur: sqlite3.Cursor, cards: dict[dict]) -> None:
+    # initialise a counter variable
+    counter = 0
+
+    #loop through all cards
+    for card in cards:
+
+        # check if cardmarket data exists for the card
+        cardmarket = card.get('cardmarket')
+
+        #if data exists, extract the relevant fields, otherwise set them to None
+        if cardmarket is not None:
+            has_reverse_holo = (cardmarket.get('prices').get('reverseHoloAvg30') != 0)
+            trend_price = cardmarket.get('prices').get('trendPrice')
+            avg30 = cardmarket.get('prices').get('avg30')
+            reverse_holo_avg30 = cardmarket.get('prices').get('reverseHoloAvg30')
+            reverse_holo_trend = cardmarket.get('prices').get('reverseHoloTrend')
+            updated_at = cardmarket.get('updatedAt')
+        else:
+            has_reverse_holo = False
+            trend_price = None
+            avg30 = None
+            reverse_holo_avg30 = None
+            reverse_holo_trend = None
+            updated_at = None   
+            
+        # refresh prices for normal variant
+        normal_variant_id = cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card['id'], 'Normal')).fetchone()[0]
+        cur.execute("INSERT INTO priceHistory (variantID, averageSellPrice, trendPrice, updatedAt,capturedAt) VALUES(?, ?, ?, ?, datetime('now'))", (normal_variant_id, avg30, trend_price, updated_at))
+        counter += 1
+
+        # if reverse holo exists, refresh prices
+        if has_reverse_holo:
+            reverse_holo_variant_id = cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card['id'], 'Reverse Holo')).fetchone()[0]
+            cur.execute("INSERT INTO priceHistory (variantID, averageSellPrice, trendPrice, updatedAt,capturedAt) VALUES(?, ?, ?, ?, datetime('now'))", (reverse_holo_variant_id, reverse_holo_avg30, reverse_holo_trend, updated_at))
+            counter += 1
+    
+    # feed back to the user how many new price records were added to the database
+    print(f"Inserted {counter} new price records into the database.")
+
+# merge latest prices with card variants
+def show_latest_prices(variants: pd.DataFrame, price_history: pd.DataFrame) -> pd.DataFrame:
+
+    # merge the two dataframes on the variantID
+    merged =  variants.merge(price_history, left_on='id', right_on='variantID', how='left')
+    latest_prices = merged.loc[merged.groupby('variantID')['capturedAt'].idxmax(), ['cardID', 'finish', 'averageSellPrice', 'trendPrice', 'capturedAt']].reset_index(drop=True)
+    return latest_prices
+
+def create_inventory(con: sqlite3.Connection) -> pd.DataFrame:
+    query = """
+        SELECT variantID, condition, SUM(qty) AS quantity_held
+        FROM (
+            SELECT variantID, purchaseCondition AS condition, purchaseQuantity AS qty
+            FROM purchases
+            UNION ALL
+            SELECT variantID, saleCondition AS condition, -saleQuantity AS qty
+            FROM sales
+        )
+        GROUP BY variantID, condition
+    """
+
+    card_variants = pd.read_sql_query("SELECT * FROM cardVariants", con).set_index('id')
+    all_cards = pd.read_sql_query("SELECT * FROM allCards", con).set_index('id')
+    listed_prices = pd.read_sql_query("SELECT * FROM listedPrices", con).set_index('id')
+
+    inventory = pd.read_sql(query, con)
+    inventory = pd.merge(inventory, card_variants, left_on='variantID', right_on='id', how='left').merge(all_cards, left_on='cardID', right_on='id', how='left').merge(listed_prices, left_on='variantID', right_on='variantID', how='left')
+
+    return inventory
+
+def get_variant_id(cur: sqlite3.Cursor, card_id: str, finish: str) -> int:
+    result = cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card_id, finish)).fetchone()
+
+    if result is None:
+        params={"select": "id,name,set,number,cardmarket,rarity"}
+        response = call_api(params, id=card_id)
+        if response.status_code != 200:
+            raise Exception(f"API error with status code {response.status_code}. Please check ID and try again")
+        
+        response_json = response.json()
+        create_new_cards(cur, response_json['data'])
+        update_prices(cur, response_json['data'])
+        result = cur.execute("SELECT id FROM cardVariants WHERE cardID = ? AND finish = ?", (card_id, finish)).fetchone()
+        if result is None:
+            raise Exception("Error with card variant. Please check finish and try again")
+
+    variant_id = result[0]
+    return variant_id
+
+def make_purchase(cur: sqlite3.Cursor, card_id: str, finish: str, quantity: int, condition: str, price: float, date, source: str) -> None:
+    variant_id = get_variant_id(cur, card_id, finish)
+    cur.execute("INSERT INTO purchases (variantID, purchaseQuantity, purchaseCondition, purchasePrice, purchaseDate, purchaseSource) VALUES (?, ?, ?, ?, ?, ?)", (variant_id, quantity, condition, price, date, source))
+
+def make_sale(cur: sqlite3.Cursor, card_id: str, finish: str, quantity: int, condition: str, price: float, date) -> None:
+    variant_id = get_variant_id(cur, card_id, finish)
+    cur.execute("INSERT INTO sales (variantID, saleQuantity, saleCondition, salePrice, saleDate) VALUES (?, ?, ?, ?, ?)", (variant_id, quantity, condition, price, date))
--- ingest_cards.py
+++ ingest_cards.py
@@ -1,33 +1,30 @@
-from dotenv import load_dotenv
-import requests
-import os
 import sqlite3
-from card_functions import update_prices, create_new_card
-
-# load .env file
-load_dotenv()
+from functions import update_prices, create_new_cards, call_api
 
 # create a connection to the database and create cursor
 con = sqlite3.connect('pokemon_tracker.db')
 cur = con.cursor()
 
-# set URL and API key
-url = "https://api.pokemontcg.io/v2/cards"
-api_key = os.getenv("POKEMON_API_KEY")
+'''
+looping using call_api test
+for i in range(1,6):
+    params={"select": "id,name,set,number,cardmarket,rarity", "page": i}
+    response_json = call_api(params)
 
-# call the API and get the response
-response = requests.get(url, headers={"X-Api-Key": api_key}, params={"select": "id,name,set,number,cardmarket,rarity"})
-response_json = response.json()
+    # insert the data
+    for item in response_json['data']:
+        create_new_card(cur, item)
+        update_prices(cur, item)
 '''
-print the first 10 items in the response for debugging purposes
-print(response_json['data'][0:10])
-'''
+        
+# define the parameters for the API call and make the call
+params={"select": "id,name,set,number,cardmarket,rarity"}
+response_json = call_api(params).json()
 
 # insert the data
-for item in response_json['data']:
+create_new_cards(cur, response_json['data'])
+update_prices(cur, response_json['data'])
 
-    create_new_card(cur, item)
-    update_prices(cur, item)
 
 # commit the changes and close the connection
 con.commit()
