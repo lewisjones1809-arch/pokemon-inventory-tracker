21da542f408cb12c172513837346aae7818640a5
Lewis Jones <lewisjones1809@gmail.com>
2026-06-23 17:42:15 +0100

    metrics updated

--- pages/1_Price_History.py
+++ pages/1_Price_History.py
@@ -31,6 +31,7 @@ price_chart = pd.read_sql_query(
     params=(int(variant_id),),
 )
 price_chart['capturedAt'] = pd.to_datetime(price_chart['capturedAt'])
+current_val = chosen_row['averageSellPrice']
 
 latest_date = price_chart['capturedAt'].max()
 seven_days_prior = latest_date - timedelta(days = 7)
@@ -38,7 +39,7 @@ thirty_days_prior = latest_date - timedelta(days = 30)
 
 seven_prior = price_chart[price_chart['capturedAt'] <= seven_days_prior].sort_values(by='capturedAt', ascending=False)
 value_seven_prior = seven_prior['averageSellPrice'].iloc[0]
-seven_perc = 100*(chosen_row['averageSellPrice'] - value_seven_prior)/value_seven_prior
+seven_perc = 100*(current_val - value_seven_prior)/value_seven_prior
 if seven_perc > 0:
     seven_sign = '+'
 else:
@@ -46,7 +47,7 @@ else:
 
 thirty_prior = price_chart[price_chart['capturedAt'] <= thirty_days_prior].sort_values(by='capturedAt', ascending=False)
 value_thirty_prior = thirty_prior['averageSellPrice'].iloc[0]
-thirty_perc = 100*(chosen_row['averageSellPrice'] - value_thirty_prior)/value_thirty_prior
+thirty_perc = 100*(current_val - value_thirty_prior)/value_thirty_prior
 if thirty_perc > 0:
     thirty_sign = '+'
 else:
@@ -54,10 +55,10 @@ else:
 
 value, seven_change, thirty_change, peak = st.columns(4)
 
-value.metric('Current Value', f'£{chosen_row['averageSellPrice']:.2f}')
-seven_change.metric('7 day % Change', f'{seven_sign}{seven_perc:.2f}%')
-thirty_change.metric('30 day % Change', f'{thirty_sign}{thirty_perc:.2f}%')
-peak.metric('Peak value', f'£{price_chart['averageSellPrice'].max():.2f}')
+value.metric('Current Value', f'£{current_val:,.2f}')
+seven_change.metric('7 day change', f'£{(current_val - value_seven_prior):,.2f}, {seven_sign}{seven_perc:,.2f}%')
+thirty_change.metric('30 day change', f'£{(current_val - value_thirty_prior):,.2f}, {thirty_sign}{thirty_perc:,.2f}%')
+peak.metric('Peak value', f'£{price_chart['averageSellPrice'].max():,.2f}')
 
 
 st.line_chart(price_chart, x='capturedAt', y='averageSellPrice', x_label='Date', y_label='Average Sale Price')
